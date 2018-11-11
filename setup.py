c6a1ef0916044272ae01c747c4c38bfcc908229d
Alisson Lauffer <alissonvitortc@gmail.com>
2018-11-11 19:40:10 -0200

    Use readme as long_description

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 import setuptools
 
+with open('README.md') as f:
+    readme = f.read()
+
 setuptools.setup(
     name='duckpy',
-    version='1.0.1',
+    version='1.1.0',
     packages=setuptools.find_packages(),
     url='https://github.com/AmanoTeam/duckpy',
     author='Amano Team',
     author_email='contact@amanoteam.ml',
     description='A simple module that searches on DuckDuckGo',
-    long_description='No description for now',
-    long_description_content_type="text/markdown"
+    long_description=readme,
+    long_description_content_type='text/markdown'
 )

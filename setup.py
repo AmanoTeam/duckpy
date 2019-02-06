d28107b19771caad686b2e47f6738b43023853e1
Alisson Lauffer <alissonvitortc@gmail.com>
2019-02-06 02:34:50 -0200

    Version 1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setuptools.setup(
     name='duckpy',
-    version='1.2.0',
+    version='1.2.1',
     packages=setuptools.find_packages(),
     install_requires=['bs4', 'urllib3', 'certifi'],
     url='https://github.com/AmanoTeam/duckpy',

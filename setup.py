178d273e1630a5721a32f9bc761ff4422c7b7ad5
Alisson Lauffer <alissonvitortc@gmail.com>
2019-06-19 23:24:06 -0300

    Declare our license on setup.py and organize items

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ with open('README.md') as f:
 
 setuptools.setup(
     name='duckpy',
-    version='1.2.2',
-    packages=setuptools.find_packages(),
+    version='1.2.3',
+
+    packages=['duckpy'],
     install_requires=['bs4', 'urllib3', 'certifi'],
+
     url='https://github.com/AmanoTeam/duckpy',
+
     author='Amano Team',
     author_email='contact@amanoteam.ml',
+
+    license='MIT',
+
     description='A simple module that searches on DuckDuckGo',
     long_description=readme,
     long_description_content_type='text/markdown'

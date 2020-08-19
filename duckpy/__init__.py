07e0130f6b71ad7f78ce230fd987f50fe3a9b4a9
Alisson Lauffer <alissonvitortc@gmail.com>
2020-08-18 21:53:54 -0300

    Fix new duckduckgo URL format

--- duckpy/__init__.py
+++ duckpy/__init__.py
@@ -46,7 +46,7 @@ def parse_page(html):
             title = i.h2.a.text
             description = i.find('a', {'class': 'result__snippet'}).text
             url = i.find('a', {'class': 'result__url'}).get('href')
-            results.append(dict(title=title, description=description, url=unquote(url[15:])))
+            results.append(dict(title=title, description=description, url=unquote(url.split("uddg=", 1)[1])))
         except AttributeError:
             pass
     return results

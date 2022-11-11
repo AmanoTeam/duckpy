3576149e01c376aa2f8ea2fb969c8eb254ed94bf
Alisson Lauffer <alissonvitortc@gmail.com>
2022-11-11 03:07:37 -0300

    chore: setup.py -> pyproject.toml and drop duckpy.aio

--- duckpy/aio/__init__.py
+++
@@ -1,6 +0,0 @@
-# Just used for backwards compatibility.
-from warnings import warn
-from .. import AsyncClient as Client
-
-
-warn("The duckpy.aio module has been deprecated in version 3.0.0 and will be removed in a future release. Please use duckpy.AsyncClient instead.", DeprecationWarning, 2)
